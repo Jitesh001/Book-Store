a9aa557c43e82aa3f3a65f0049e6696088ccd70c
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-09-13 19:50:27 +0530

    updated github action

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@
 TAG=${1:-"dev"}
 
 # Replace the tag in the docker-compose.yaml file
-sed -i "s@\(image: pixiejitesh/bookstore-backend:\).*\$@\1$TAG@" docker-compose.yaml
+# sed -i "s@\(image: pixiejitesh/bookstore-backend:\).*\$@\1$TAG@" docker-compose.yaml
+sed -i '' "s@\(image: pixiejitesh/bookstore-backend:\).*\$@\1$TAG@" docker-compose.yaml
 
 # Generate the doppler.env file
 doppler secrets download --no-file --format env --project book_store --config dev_jitesh > doppler.env
 
 # Deploy the Image for the branch specified
-docker-compose pull && docker-compose down && doppler run -- docker-compose up -d
+docker compose pull && docker compose down && doppler run -- docker compose up -d
